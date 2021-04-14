0db71b345a06669c6cc80fa81b3484282031a8ea
Jalisa <jalisa_calhoun@yahoo.com>
2021-04-13 18:39:54 -0700

    Changes Made

---
+++ Procfile
@@ -0,0 +1,2 @@
+release: python manage.py migrate
+web: gunicorn your_project_name.wsgi --log-file -
--- capstone/vegetableGame/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class VegetablegameConfig(AppConfig):
-    name = 'vegetableGame'
